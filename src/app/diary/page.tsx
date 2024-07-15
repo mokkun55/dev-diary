60f1040e096f08e48a7ba6878c1383d4d16598ca
mokkun55 <mokkunpc@gmail.com>
2024-07-15 18:02:37 +0900

    いまから日記一覧作る

--- src/app/diary/page.tsx
+++ src/app/diary/page.tsx
@@ -1,21 +1,24 @@
+"use client";
+
 import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
-import React from "react";
+import Diary from "@/components/types";
+import React, { useState } from "react";
 
 // 過去の日記を一覧で見れるページ
 // クリックで "diary/[id]" に飛ばす
-function page() {
+function Page() {
+  const [diarys, setDiarys] = useState<Diary[]>([]);
   return (
     <>
       <Header />
-      <div className="flex">
-        <Sidebar />
+
+      <div className="m-4">
         <div className="w-full">
-          <h1 className="">日記一覧</h1>
+          <h1 className="text-center text-3xl my-4 font-bold">日記一覧</h1>
         </div>
       </div>
     </>
   );
 }
 
-export default page;
+export default Page;
