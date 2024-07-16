676ceeb2ccd233add33054a875bcfffe1f1dcafa
mokkun55 <mokkunpc@gmail.com>
2024-07-16 15:45:58 +0900

    ログインしないとルートページ表示させない

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,40 @@
+"use client";
+
+import Btn from "@/components/Btn";
 import Header from "@/components/Header";
 import InputArea from "@/components/InputArea";
 import Sidebar from "@/components/Sidebar";
+import { auth } from "@/firebase";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function Page() {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
   return (
     <>
       <Header />
       <div className="flex h-[calc(100vh-80px)]">
-        <Sidebar />
         {/* TODO 今日の日記を書いている場合 今日の日記を表示する */}
         {/* きほん1日1日記 */}
-        <InputArea />
+
+        {user ? (
+          <>
+            <Sidebar />
+            <InputArea />
+          </>
+        ) : (
+          <div className="text-center w-full mt-12">
+            <h1 className="text-5xl font-bold">ログインしてください</h1>
+            <Btn
+              className="text-2xl bg-blue-500 hover:bg-blue-700 mt-4 w-[250px] h-[70px]"
+              onClick={() => router.push("/login")}
+            >
+              ログインページへ
+            </Btn>
+          </div>
+        )}
       </div>
     </>
   );
